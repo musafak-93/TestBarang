acf322b8c9aee0f2e47fd25649b9a5b850afc0a3
Musafak <musafak.ft18@mail.umy.ac.id>
2020-06-10 09:46:32 +0700

    inisialisasi variable yang dipakai

--- app/src/main/java/com/example/testbarang/LihatBarang.java
+++ app/src/main/java/com/example/testbarang/LihatBarang.java
@@ -1,10 +1,21 @@
 package com.example.testbarang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
 public class LihatBarang extends AppCompatActivity {
+    // Inisialisasi variable yang dipakai
+    private DatabaseReference database;
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Barang> daftarBarang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
